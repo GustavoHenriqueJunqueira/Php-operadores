3ceabd46f851c3c8c63d7d24b948b68e7e333efc
Gustavo Junqueria <gustavohenriquejunqueira@hotmail.com>
2018-07-25 10:17:14 -0300

    Operadores aritméticos. ( + - * / % ** )
    * echo $a + $b;  (SOMA)
    * echo $a - $b;  (SUBTRAÇÃO)
    * echo $a * $b;  (MULTIPLICAÇÃO)
    * echo $a / $b;  (DIVISÃO)
    * echo $a % $b; (MODÚLO)
    * echo $a ** $b; (EXPONENCIAÇÃO)

---
+++ Operadores/exemplo-03.php
@@ -0,0 +1,28 @@
+<?php
+
+$a =10;
+$b =2;
+
+echo $a + $b; //(SOMA)
+
+echo"<br/>";
+
+echo $a - $b; //(SUBTRAÇÃO) 
+
+echo"<br/>";
+
+echo $a * $b; //(MULTIPLICAÇÃO)
+
+echo"<br/>";
+
+echo $a / $b; //(DIVISÃO)
+
+echo"<br/>";
+
+echo $a % $b; //(MODÚLO)
+
+echo "<br/>";
+
+echo $a ** $b; //(EXPONENCIAÇÃO)
+
+?>
