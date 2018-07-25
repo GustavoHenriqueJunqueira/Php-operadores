73b8615ce38a0b23f9974648e3258864afadda12
Gustavo Junqueria <gustavohenriquejunqueira@hotmail.com>
2018-07-25 13:09:24 -0300

    Operadores de comparação.
        * Os operadores de comparação vão ser uteis ao comparar qual valor é maior ou menos.
    
                           Ex: var_dump($a  >  $b);
    
                Está comparando a o valor da variável a com o valor da variável b.
    
                        Ex: var_dump($a < $b);
    
                    Está comparando o valor da variavam a para ver se a variável b é maior.
    
                    Ex: var_dump($a == $b);
    
                    Está verificando de ambos os valores dentro das variáveis são iguais LEMBRANDO QUE DEVE SER 2 ==.
    
                    Ex: var_dump($a === $b);
    
                    Está sendo verificado tanto o valor dentro das variáveis quanto o próprio tipo da variável.

---
+++ Operadores/exemplo-04.php
@@ -0,0 +1,26 @@
+<?php
+
+$a =40.0;
+$b = 55;
+
+var_dump($a > $b);  //Menor
+
+echo"<br/>";
+
+var_dump($a < $b); //Maior
+
+echo"<br/>";
+
+var_dump ($a == $b); //Valores Iguais
+
+echo "<br/>";
+
+var_dump($a === $b); //Valores e Tipos 
+
+echo "<br/>";
+
+var_dump($a != $b); // Diferente
+
+echo "<br/>";
+
+?>
