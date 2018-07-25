3b82baad99148b5863b227dfb43693c031737128
Gustavo Junqueria <gustavohenriquejunqueira@hotmail.com>
2018-07-25 10:16:08 -0300

    Atribuição VALOR
        * Operador ( + = ) Neste caso você consegue ir acrescentando na variável um novo valor e ir soltando automaticamente, no final será realizado a soma e exibido a soma.
    
                            Ex; $valorTotal = 0; (Iniciou a variável atribuído o valor de 0)
                                  $valorTotal +=100; (Adicionou 100 na onde tinha 0)
                                  $valorTotal +=25; (Adicionou 25 na onde tinha 100)
                         echo $valorTotal;   (Total exibido na tela será de 125).
    
        *  Desta mesma forma mostrada antes é possível utilizar o ( - = ) para que haja subtração.
                           Ex ; $valorTotal -=10; (Subtraiu de 125 que já tinha antes na variável uma quantidade 10);
    
        * Alem desta questão é possível dar descontos utilizando a %.
    
                           Ex; $valorTotal *=.9; (Desta forma será aplicado um desconto como porcentagem)

---
+++ Operadores/exemplo-02.php
@@ -0,0 +1,15 @@
+<?php
+
+$valorTotal = 0; //(Iniciou a variável atribuído o valor de 0)
+
+$valorTotal+= 100; //(Adicionou 100 na onde tinha 0)
+
+$valorTotal += 25;//(Adicionou 25 na onde tinha 100)
+
+$valorTotal -= 10;//(Subtraiu de 125 que já tinha antes na variável uma quantidade 10);
+
+$valorTotal *= .9; //(Desta forma será aplicado um desconto como porcentagem)
+
+echo $valorTotal;
+
+?>
